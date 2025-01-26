87e5bd371f2be4d51f61bd3aec35b4df9a7445b4
Olga Volobueva <olvik1605@gmail.com>
2025-01-26 16:47:41 -0500

    added Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Button from "./components/Button/Button";
+import Input from "./components/Input/Input";
 
 function App() {
     return (
         <>
             <Button onClick={() => console.log("Button")}>Add</Button>
+            <Input isValid />
         </>
     );
 }
