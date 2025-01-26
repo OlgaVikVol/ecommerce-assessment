a1a6b446aa5ccd55848aeef021628f6eb8988956
Olga Volobueva <olvik1605@gmail.com>
2025-01-26 17:52:19 -0500

    added Layout, Outlet, router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Menu from "./pages/Menu/Menu";
+import Cart from "./pages/Cart/Cart";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import Layout from "./layout/Menu/Menu.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <Menu />,
+            },
+            {
+                path: "/cart",
+                element: <Cart />,
+            },
+        ],
+    },
+    {
+        path: "*",
+        element: <ErrorPage />,
+    },
+]);
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+        <App />
+    </StrictMode>
+);
